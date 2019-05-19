dd782a651403b4f4a7af18f071cde1192270dae1
icsantos <isantos1206@gmail.com>
2019-05-19 01:05:05 -0500

    feat: GET images and articles using Ajax and XHR

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,12 +1,57 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
     const responseContainer = document.querySelector('#response-container');
+    let searchedForText = '';
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID <client-id>');
+        unsplashRequest.send();
+
+        const articleRequest = new XMLHttpRequest();
+
+        articleRequest.onload = addArticles;
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<api key>`);
+        articleRequest.send();
     });
+
+    function addImage() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if (data && data.results && data.results[0]) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = data.response.docs.map((article) => `<li class="article"><h2><a href="${article.web_url}">${article.headline.main}</a></h2><p>${article.snippet}</p>
+                </li>`).join('');
+            htmlContent = `<ul>${htmlContent}</ul>`;
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
 })();
